71f11046145b0b778805e9bea0d671e087eb7eae
evanjenkinssss <evanwales123@hotmail.co.uk>
2023-09-13 20:00:55 +0100

    Implemented the chart into maindisplay

--- expensetracker/src/App.js
+++ expensetracker/src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
 import Maindisplay from './components/maindisplay';
 
 
+
+
+
 function App() {
+
+  
+
   return (
     <div>
       <Header /> 
       <Maindisplay /> 
+
 
     </div>
 
